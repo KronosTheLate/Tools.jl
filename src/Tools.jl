aada79ac2be0fb05e168f6342ba2be963dcbca74
Dennis Bal <densbal@live.no>
2022-03-16 14:32:44 +0100

    add print_fields

--- src/Tools.jl
+++ src/Tools.jl
@@ -1,5 +1,17 @@
 module Tools
 
-# Write your package code here.
+"""
+    print_fields(x, l_max=500, padding=15, padfunc=lpad)
+
+Print all fieldnames of `x`, followed their value.
+"""
+function print_fields(x, l_max=500, padding=15, padfunc=lpad)
+    fields = fieldnames(x)
+    for field in fields
+        second_part = "$(getfield(x, field))"
+        second_part = length(second_part) < l_max ? second_part : "More than $l_max letters."
+        println(padfunc("$field = ", padding) *  second_part)
+    end
+end
 
 end
